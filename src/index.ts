8894aa4368f992fb7a63652a160f17fca5a0bb22
madhuwantha <eng.madhuwanthapriyashan@gmail.com>
2025-06-29 12:07:53 +0530

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import cors from 'cors'
+
 const app = express()
 app.use(express.json());
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 
 const port = 3000
