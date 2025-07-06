64429abff2980059ff8c8e7cc2bc31320ca71ac9
madhuwantha <eng.madhuwanthapriyashan@gmail.com>
2025-07-06 10:09:36 +0530

    change the port

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ app.use(express.json());
 // app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 
-const port = 3000
+const port = process.env.PORT || 3000
 const uri = process.env.MONOGODB_URL|| ""
 
 mongoose.connect(uri).then(() => {
